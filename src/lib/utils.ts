7caa59b034d8bcfd6dd882c515c85a5f95842b85
Saksham Shah <saksham101@gmail.com>
2026-03-19 00:24:00 +0000

    lint

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// eslint-disable-next-line no-explicit-any
 export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs));
 }
